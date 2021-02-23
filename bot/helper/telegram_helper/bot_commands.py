150c75ee1e30ad28e945a41d9163d49eb249f76d
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-02-23 16:14:56 +0800

    Add files via upload

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,24 +1,24 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
-        self.UnzipMirrorCommand = 'unzipmirror'
-        self.TarMirrorCommand = 'tarmirror'
-        self.CancelMirror = 'cancel'
-        self.CancelAllCommand = 'cancelall'
-        self.ListCommand = 'list'
-        self.SpeedCommand = 'speedtest'
+        self.StartCommand = 'mulai'
+        self.MirrorCommand = 'kaca'
+        self.UnzipMirrorCommand = 'bukaarsip'
+        self.TarMirrorCommand = 'tarkaca'
+        self.CancelMirror = 'gagal'
+        self.CancelAllCommand = 'batalin'
+        self.ListCommand = 'daftar'
+        self.SpeedCommand = 'testkecepatan'
         self.StatusCommand = 'status'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
         self.StatsCommand = 'stats'
-        self.HelpCommand = 'help'
+        self.HelpCommand = 'tolong'
         self.LogCommand = 'log'
-        self.CloneCommand = "clone"
-        self.WatchCommand = 'watch'
+        self.CloneCommand = "cln"
+        self.WatchCommand = 'nonton'
         self.TarWatchCommand = 'tarwatch'
-        self.deleteCommand = 'del'
+        self.deleteCommand = 'hapus'
 
 BotCommands = _BotCommands()
