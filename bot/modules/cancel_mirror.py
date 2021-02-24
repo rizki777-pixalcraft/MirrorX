74a8f3f004b0a367e37766c1e39a2e92be1cc8bb
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-02-24 23:28:35 +0800

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -18,7 +18,7 @@ def cancel_mirror(update, context):
         gid = args[1]
         dl = getDownloadByGid(gid)
         if not dl:
-            sendMessage(f"GID: <code>{gid}</code> not found.", context.bot, update)
+            sendMessage(f"ID yang kamu mirror: <code>{gid}</code> not found.", context.bot, update)
             return
         with download_dict_lock:
             keys = list(download_dict.keys())
@@ -32,18 +32,18 @@ def cancel_mirror(update, context):
         if mirror_message is None or mirror_message.message_id not in keys:
             if BotCommands.MirrorCommand in mirror_message.text or \
                     BotCommands.TarMirrorCommand in mirror_message.text:
-                msg = "Mirror already have been cancelled"
+                msg = "Mirror nya udah aku berhentiin ya"
                 sendMessage(msg, context.bot, update)
                 return
             else:
-                msg = "Please reply to the /mirror message which was used to start the download or /cancel gid to cancel it!"
+                msg = "Kalo pengen nge mirror pake /kaca sama kalo pengen batalin pake /gagal"
                 sendMessage(msg, context.bot, update)
                 return
-    if dl.status() == "Uploading":
-        sendMessage("Upload in Progress, Don't Cancel it.", context.bot, update)
+    if dl.status() == "Lagi upload":
+        sendMessage("Lagi Proses upload jangan digagalin.", context.bot, update)
         return
-    elif dl.status() == "Archiving":
-        sendMessage("Archival in Progress, Don't Cancel it.", context.bot, update)
+    elif dl.status() == "Lagi Nge Arsip":
+        sendMessage("Lagi Nge arsip jangan dibatalin.", context.bot, update)
         return
     else:
         dl.download().cancel_download()
