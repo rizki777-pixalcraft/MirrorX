bacd5a4d0af9a4daaccec7a1e5d1d56b11820fd0
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-02-24 22:43:38 +0800

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,9 +17,10 @@ def _watch(bot: Bot, update, isTar=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [yt_dl supported link] [quality] |[CustomName] to mirror with youtube_dl.\n\n"
-        msg += "<b>Note :- Quality and custom name are optional</b>\n\nExample of quality :- audio, 144, 240, 360, 480, 720, 1080, 2160."
-        msg += "\n\nIf you want to use custom filename, plz enter it after |"
+        msg = f"/{BotCommands.WatchCommand} [Bisa pake link youtube] [Kualitas] |[Kustomnama] Biar di mirror in pake link youtube.\n\n"
+        msg += "<b>Note :- Kualitas dan kustom nama itu pelengkap aja ya</b>\n\nMisalkan aja :- audio, 144, 240, 360, 480, 720, 1080, 2160."
+        msg += "\n\nIf you want to use custom filename, plz enter it after
+|"
         msg += f"\n\nExample :-\n<code>/{BotCommands.WatchCommand} https://youtu.be/ocX2FN1nguA 720 |My video bro</code>\n\n"
         msg += "This file will be downloaded in 720p quality and it's name will be <b>My video bro</b>"
         sendMessage(msg, bot, update)
