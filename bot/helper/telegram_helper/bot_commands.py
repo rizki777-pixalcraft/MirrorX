8c646e7e83f952e15c14c52140ad77a54b16c25b
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2021-02-26 22:34:14 +0800

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -20,6 +20,5 @@ class _BotCommands:
         self.WatchCommand = 'nonton'
         self.TarWatchCommand = 'tarwatch'
         self.deleteCommand = 'hapus'
-        self.Clncommand
         
 BotCommands = _BotCommands()
